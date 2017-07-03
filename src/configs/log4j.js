7d0bdf89862627570b48f252ab6c1b79c454ee58
z008hy <z008hy@163.com>
2017-07-03 17:36:24 +0800

    项目启动标示

--- src/configs/log4j.js
+++ src/configs/log4j.js
@@ -11,14 +11,14 @@ export default {
     [
       {
         category: 'errorLogger',             //logger名称
-        type: 'DateFile',                   //日志类型
+        type: 'dateFile',                   //日志类型
         filename: errorPath,             //日志输出位置
         alwaysIncludePattern: true,          //是否总是有后缀名
         pattern: '-yyyy-MM-dd.log',          //后缀，每小时创建一个新的日志文件
       },
       {
         category: 'infoLogger',
-        type: 'DateFile',
+        type: 'dateFile',
         filename: infoPath,
         alwaysIncludePattern: true,
         pattern: '-yyyy-MM-dd.log',
--- src/configs/mongo.js
+++ src/configs/mongo.js
@@ -2,7 +2,7 @@
  * Created by zhanghaoyi on 17/6/27.
  */
 export default {
-  mongodbUrl: '',
+  mongodbUrl: 'mongodb://47.92.80.169:27017/blog',
   option: {
     useMongoClient : true,
   },
