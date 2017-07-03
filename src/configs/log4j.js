d8afeca45a1ccc79d91ded3eedee96a9b816a376
z008hy <z008hy@163.com>
2017-07-03 17:28:30 +0800

    项目启动标示

--- src/configs/log4j.js
+++ src/configs/log4j.js
@@ -11,7 +11,7 @@ export default {
     [
       {
         category: 'errorLogger',             //logger名称
-        type: 'console',                   //日志类型
+        type: 'DateFile',                   //日志类型
         filename: errorPath,             //日志输出位置
         alwaysIncludePattern: true,          //是否总是有后缀名
         pattern: '-yyyy-MM-dd.log',          //后缀，每小时创建一个新的日志文件
